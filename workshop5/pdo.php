4797eea8e419a7f4ed7d3b48a8eda69e759c8243
Michelle <40885572+ombidmichelle@users.noreply.github.com>
2018-09-08 17:33:54 +0800

    Delete pdo.php

--- workshop5/pdo.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo "<pre>\n";
-try {
-    $pdo=new PDO('mysql:host=sql12.freemysqlhosting.net;dbname=sql12218082', 'sql12218082', '9HaaYaV9gz');
-} catch (PDOException $e) {
-    print "Error!: " . $e->getMessage() . "<br/>";
-    die();
-}
-echo "</pre>\n";?>
